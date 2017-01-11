1cb07e207900c15547dbf286166db6638afa37b2
Soja <domain.xh@gmail.com>
2017-01-10 20:27:06 -0500

    Initial Commit

---
+++ Social_Network_Firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social_Network_Firebase
+//
+//  Created by Xiaoheng Pan on 1/10/17.
+//  Copyright © 2017 Xiaoheng Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
